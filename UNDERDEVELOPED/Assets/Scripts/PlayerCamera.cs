ba83cbe5818c89af1b7967cfd7d9dd7d587d5749
UlgyAndFart <168442396+UglyAndFart@users.noreply.github.com>
2024-08-14 14:30:29 +0800

    Update Comlab Collision

--- UNDERDEVELOPED/Assets/Scripts/PlayerCamera.cs
+++ UNDERDEVELOPED/Assets/Scripts/PlayerCamera.cs
@@ -4,25 +4,22 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    public Transform target;
-    public float lerpSpeed = 1.0f;
-
-    private Vector3 offset = new Vector3(0,1,0);
-
-    private Vector3 targetPos;
+    [SerializeField]
+    private GameObject mainCamera;
+    [SerializeField]
+    private float lerpSpeed = 1.0f;
 
     private void Start()
     {
-        if (target == null) return;
+        if (mainCamera == null) return;
         
-        transform.position = target.position;
+        mainCamera.transform.position = new Vector3 (transform.position.x, transform.position.y, mainCamera.transform.position.z);
     }
 
     private void Update()
     {
-        if (target == null) return;
+        if (mainCamera == null) return;
 
-        targetPos = target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, targetPos, lerpSpeed * Time.deltaTime);
+        mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3 (transform.position.x, transform.position.y, mainCamera.transform.position.z), lerpSpeed * Time.deltaTime);
     }
 }
