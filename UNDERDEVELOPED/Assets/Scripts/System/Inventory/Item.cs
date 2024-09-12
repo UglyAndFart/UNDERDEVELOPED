4ee3884b1c76e55bb49020ffeb2e1c4703b53b73
Meinard Legashki Limlengco <meliorasimp@gmail.com>
2024-09-12 12:56:23 +0800

    Revert "Added Inventory UI with Rough Logic"
    This reverts commit 64118cdff3ae46f3a380d65a1d230452818de159.

--- UNDERDEVELOPED/Assets/Scripts/System/Inventory/Item.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
-public class Item : ScriptableObject
-{
-    new public string _name = "New Item";
-    public Sprite _icon = null;
-    public bool defaultItem = false;
-}
